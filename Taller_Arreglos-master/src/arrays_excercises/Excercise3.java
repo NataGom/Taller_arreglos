a8ec5f77ef7fec9721c6f676013b2360d254fe79
NataGom <nataliagomezramos439@gmail.com>
2025-10-15 16:27:20 -0500

    Add files via upload

---
+++ Taller_Arreglos-master/src/arrays_excercises/Excercise3.java
@@ -0,0 +1,10 @@
+package arrays_excercises;
+import javax.swing.JOptionPane;
+import utils.Utility;
+
+public class Excercise3 {
+    public static void excercise3() {
+        int size = Utility.readInteger("Ingrese el tamaño del arreglo");
+        int[] array = Utility.readIntegerArray("Ingrese los datos", size);
+    }
+}
